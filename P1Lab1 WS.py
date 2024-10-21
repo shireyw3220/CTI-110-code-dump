13142cddee00caf91d001eb5da6de9e7b3303f08
shireyw3220 <shireyw3220@student.faytechcc.edu>
2024-10-20 23:29:45 -0400

    Add files via upload

---
+++ P1Lab1 WS.py
@@ -0,0 +1,9 @@
+# William S
+# 10/20/2024 (sorry)
+# P1LAB1
+# Short program that prints a user input and welcomes them to the course.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110-0903!")
